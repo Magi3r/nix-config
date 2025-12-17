562b8623a6245bd9970a855a79ad86be4e42ffc2
Magi3r <jvd@outlook.de>
2025-12-17 03:52:02 +0100

    feat(zellij): Enable mouse mode

--- home/zellij/default.nix
+++ home/zellij/default.nix
@@ -3,13 +3,13 @@
   programs.zellij = {
     enable = true;
     enableBashIntegration = true;
-#     enableZshIntegration = true;
+    #     enableZshIntegration = true;
     enableFishIntegration = true;
 
     settings = {
       theme = "catppuccin-mocha-lavender";
       copy_command = "wl-copy";
-      mouse_mode = false;
+      mouse_mode = true;
 
       show_startup_tips = true;
       show_release_notes = false;
@@ -61,33 +61,32 @@
   # config file”, which in this case is .zshrc.
   # This config can be done in ../shell/default.nix too, but it's better here
   # It's same as enableZshIntegration but custom
-#   programs.zsh.initContent = lib.mkAfter ''
-#     function zellij_tab_name_update() {
-#       if [[ -n $ZELLIJ ]]; then
-#         current_dir=$PWD
-#
-#         case "$current_dir" in
-#           "$HOME")            current_dir=" " ;;
-#           "$HOME/Downloads")  current_dir=" " ;;
-#           "$HOME/Music")      current_dir=" " ;;
-#           "$HOME/Pictures")   current_dir=" " ;;
-#           "$HOME/Videos")     current_dir=" " ;;
-#           "$HOME/Documents")  current_dir=" " ;;
-#           "$HOME/Documents/code")  current_dir=" " ;;
-#           "$HOME/Documents/code/dotfiles")   current_dir="󱄅 " ;; # you would want to change this
-#           *)
-#             current_dir=''${current_dir##*/}
-#             ;;
-#         esac
-#
-#         command nohup zellij action rename-tab "$current_dir" >/dev/null 2>&1
-#       fi
-#     }
-#
-#     zellij_tab_name_update
-#     autoload -Uz add-zsh-hook
-#     add-zsh-hook chpwd zellij_tab_name_update'';
-
+  #   programs.zsh.initContent = lib.mkAfter ''
+  #     function zellij_tab_name_update() {
+  #       if [[ -n $ZELLIJ ]]; then
+  #         current_dir=$PWD
+  #
+  #         case "$current_dir" in
+  #           "$HOME")            current_dir=" " ;;
+  #           "$HOME/Downloads")  current_dir=" " ;;
+  #           "$HOME/Music")      current_dir=" " ;;
+  #           "$HOME/Pictures")   current_dir=" " ;;
+  #           "$HOME/Videos")     current_dir=" " ;;
+  #           "$HOME/Documents")  current_dir=" " ;;
+  #           "$HOME/Documents/code")  current_dir=" " ;;
+  #           "$HOME/Documents/code/dotfiles")   current_dir="󱄅 " ;; # you would want to change this
+  #           *)
+  #             current_dir=''${current_dir##*/}
+  #             ;;
+  #         esac
+  #
+  #         command nohup zellij action rename-tab "$current_dir" >/dev/null 2>&1
+  #       fi
+  #     }
+  #
+  #     zellij_tab_name_update
+  #     autoload -Uz add-zsh-hook
+  #     add-zsh-hook chpwd zellij_tab_name_update'';
 
   home.packages = with pkgs;
     [
